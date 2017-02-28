f2c22f0578d2ff084823a707fb57dc6aa610e672
Boshika Tara <boshikatara@gmail.com>
2017-02-28 13:05:12 -0800

    adds stack interface

---
+++ Input_Files/src/ADT.java
@@ -0,0 +1,16 @@
+/** Interface for the class Stack
+ *
+ *
+ * @author Boshika Tara
+ **/
+
+public abstract interface ADT{
+    public boolean isEmpty();
+
+    public void push(String value);
+
+    public String pop();
+
+    public String peek();
+
+}
